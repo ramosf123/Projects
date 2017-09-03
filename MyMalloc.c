3b517712877ffed85779c391e6a1d2c3771b4767
Farid <ramosf@purdue.edu>
2017-09-03 19:57:05 -0400

    Commit

--- MyMalloc.c
+++ MyMalloc.c
@@ -225,6 +225,8 @@ static void freeObject(void *ptr)
             //Get the object ahead of the rightNgbr to change its leftObjectSize
             FreeObject * nextToRight = (FreeObject *)((char *) rightNgbr + getSize(&rightNgbr->boundary_tag));
             nextToRight->boundary_tag._leftObjectSize = newSize;
+            curr->free_list_node._prev = rightNgbr->free_list_node._prev;
+            curr->free_list_node._next = rightNgbr->free_list_node._next;
             rightNgbr->free_list_node._prev->free_list_node._next = curr;
             rightNgbr->free_list_node._next->free_list_node._prev = curr;
             return;
